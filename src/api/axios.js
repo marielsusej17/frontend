2d1be815f59e0d3ba09bf142696a7d2f1f0f8c5f
marielsusej17 <17mariel122007@gmail.com>
2026-04-16 16:17:01 -0500

    correcio

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // ✅ FIX: evitar undefined en Vercel
 const API_URL =
   import.meta.env.VITE_API_URL ??
-  "https://backend-z35t.onrender.com/api/vehiculos";
+  "https://backend-z35t.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
